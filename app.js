70cfc3bb440e4997163e5d81c91dc2ddc18d527e
abhinav.r <abhinav.r@go-jek.com>
2017-06-27 13:10:07 +0530

    modified python script

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-
+'use strict'
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var request = require('request');
+var hapi = require('hapi');
+var dotenv = require('dotenv');
+dotenv.load();
 
-var TIME = 0;
+
+//var TIME = 0;
 
 
 app.get('/', function(req, res){
@@ -13,10 +17,12 @@ app.get('/', function(req, res){
 
 //console.log(__dirname); -> /Users/admin/Documents/git_repos/test_socketIo
 const Influx = require('influx');
-
+let pool = Influx.pool;
 const influx = new Influx.InfluxDB({
+  username: 'abhinav',
+  password: '',
   host: 'localhost',
-  database: 'bigsupply',
+  database: 'testDb',
   schema: [
     {
       measurement: 'supply_data',
@@ -36,58 +42,62 @@ var users = 0;
 io.on('connection', function(socket){
   users++;
   console.log('A user connected'+users);
+  console.log(process.env.DB_HOST);
   var val;
-  socket.on('clientData',function(data){
-    var query = "select * from supply_data where vehicle_type='BIKE' limit "+data;
-
-        //***********************************************
-    //var url="http://localhost:8086/query";
-    //var propertiesObject = { db:'bigsupply', q:query };
-    //request({url:url, qs:propertiesObject}, function(err, response, body) {
-      //if(err) { console.log(err); return; }
-      //val=response.body;
-    //});
-    //***********************************************
-
-    //Or
-
-    //***********************************************
+  socket.on('clientData',function(data,callback){
+    console.log(data);
+    var query = "select* from supply_data limit 1 ";
+    var window1=1000;
     influx.query(query).then(results => {
-      val = results;
+      console.log(results.length);
+      callback('server data received callback');
+      socket.emit('dbData',JSON.stringify(results,null,2));
     })
-    // **********************************************
+      .catch(err => {
+        console.error(err);
+      })
 
   });
-
-  setInterval(function(){
-		////////Sending an object when emmiting an event
-    socket.emit('dbData',JSON.stringify(val,null,2));
-	}, TIME);
-
-  ////////Whenever someone disconnects this piece of code executed
   socket.on('disconnect', function () {
     console.log('A user disconnected');
     users--;
   });
+
+  socket.on('connect_failed', function(){
+    console.log('Connection Failed');
+  });
+
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-//var app = require('express')();
-//var http = require('http').Server(app);
-//var io = require('socket.io')(http);
 
-//app.get('/', function(req, res){
-  //res.sendFile('index.html', { root: __dirname });
-//});
 
-//var nsp = io.of('/my-namespace');
-//nsp.on('connection', function(socket){
-  //console.log('someone connected');
-  //nsp.emit('hi', 'Hello everyone!');
-//});
-//http.listen(3000, function(){
-  //console.log('listening on localhost:3000');
-//})
-;
+
+
+
+
+
+
+
+// val = results;
+// var l = val.length;
+// var result = [];
+// for (var i=0;i<l;)
+// {
+//   var drivers = 0;
+//   for( var j=0;j<window1;j++)
+//   {
+//     if(i+j==l)
+//       break;
+//     drivers += val[i+j].unique_drivers;
+//   }
+//   drivers=drivers/window1;
+//   i=i+window1;
+//   var obj = {
+//     driver: drivers,
+//     time: i+window1/2
+//   };
+//   result.push(obj);
+// }
--- influx_injection_script.py
+++ influx_injection_script.py
@@ -8,17 +8,18 @@ import argparse
 from influxdb import client as influxdb
 
 
-db = influxdb.InfluxDBClient(host='127.0.0.1',port = 8086, database = 'supplydata')
+db = influxdb.InfluxDBClient(host='127.0.0.1',port = 8086, database = 'supply_data')
 
 def read_data():
-    with open('supply_data.csv') as f:
+    with open('extern/supply_data.csv') as f:
         return [x.split(',') for x in f.readlines()[1:]]
 
 a = read_data()
 
 for metric in a:
     # print metric
-    x = int(metric[0])
+    x = int(metric[0])*1000000000;
+    #x = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(x))
     y = int(metric[1])
     influx_metric = [{
         'measurement': 'supply_data',
@@ -26,11 +27,9 @@ for metric in a:
         'tags': {
             's2_id_level': metric[2],
             's2_id' : metric[3],
-            'vehicle_type' : metric[4],
-            'duration': y-x
+            'vehicle_type' : metric[4]
             },
         'fields': {
-            'end_time': y,
             'unique_drivers' : int(metric[5].strip())
             }
         }
